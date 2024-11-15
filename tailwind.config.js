cd27c80019f445b23b92e97058179ce018b29494
mahdi <mahdideveloper80@gmail.com>
2024-11-15 21:36:34 +0330

    Start Project :)

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        snapp: "#21AA58",
+      },
+      fontFamily: {
+        iransans: ["iransans"],
+      },
+    },
+  },
+  plugins: [],
+};
